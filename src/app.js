9d69ba7c21fedeb1cec458b1568d50e5ff2b7327
Piyush Abbi <piyushabbi27@gmail.com>
2017-11-16 15:29:02 +0530

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import 'normalize-css/normalize.css';
+import './styles/styles.scss';
+
+render(<h1>Hello</h1>, document.getElementById('root'));
